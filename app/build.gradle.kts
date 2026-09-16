962522d90d72fe565b6b17c2fa34dc69d34e411c
喵呜Miaowu <82693744+MiaoWuNYA@users.noreply.github.com>
2026-09-16 09:53:37 +0800

    chore: bump version to 2.5.4 (214)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -56,8 +56,8 @@ android {
         applicationId = "me.rerere.rikkahub"
         minSdk = 26
         targetSdk = 37
-        versionCode = 213
-        versionName = "2.5.3fix13"
+        versionCode = 214
+        versionName = "2.5.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
