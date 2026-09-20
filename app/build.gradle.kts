4a50b7c6a49f5b3deb9a23b60df6467e9e8244ec
喵呜Miaowu <82693744+MiaoWuNYA@users.noreply.github.com>
2026-09-20 11:50:04 +0800

    chore: bump version to 2.5.4fix11 (224)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -56,8 +56,8 @@ android {
         applicationId = "me.rerere.rikkahub"
         minSdk = 26
         targetSdk = 37
-        versionCode = 223
-        versionName = "2.5.4fix10"
+        versionCode = 224
+        versionName = "2.5.4fix11"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
